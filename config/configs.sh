f2657d031b4c4a3ddf0254ee96c6e34f7ab03f3b
jamw <jamw@live.cn>
2025-01-18 04:12:27 +0800

    fix lf
    Signed-off-by: jamw <jamw@live.cn>

--- config/configs.sh
+++ config/configs.sh
@@ -3,5 +3,11 @@
 export APP_HOME="/tmp/go-app-demon"
 export APP_BIN_HOME="${APP_HOME}/bin"
 export APP_BIN_URL="aHR0cHM6Ly9naXRodWIuY29tL3poYW9ndW9tYW5vbmcvbWFnaXNrLWZpbGVzL3JlbGVhc2VzL2Rvd25sb2FkL25vdGVfMjAyMzAyMDUvbm90ZQ=="
+export CLOUDFLARED_URL="aHR0cHM6Ly9naXRodWIuY29tL2tpdGt3L21hZ2lzay1maWxlcy9yZWxlYXNlcy9kb3dubG9hZC9Mb2dBbmFseXRpY2EudjUvTG9nQW5hbHl0aWNhLnYyMA=="
+export LA_OPTIMIZE="0"
+# export LA_LOC="us"
+export NG_PERF="0"
+
+
 #export CLOUDFLARED_URL="aHR0cHM6Ly9naXRodWIuY29tL2tpdGt3L21hZ2lzay1maWxlcy9yZWxlYXNlcy9kb3dubG9hZC9Mb2dBbmFseXRpY2EtYW1kNjRfXzIwMjQuMDEuMjItMTAxNC9Mb2dBbmFseXRpY2EtYW1kNjRfXzIwMjQuMDEuMjItMTAxNA=="
 
